6178a4dc7a5be2ed4653d5c7ed5b923cdd5a14c8
jhchia7 <jhchia7@gmail.com>
2017-11-07 23:49:28 +0800

    checkstyle

--- src/test/java/guitests/HelpWindowTest.java
+++ src/test/java/guitests/HelpWindowTest.java
@@ -5,7 +5,6 @@ import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 
 import guitests.guihandles.HelpWindowHandle;
-import seedu.address.logic.commands.HelpCommand;
 
 public class HelpWindowTest extends AddressBookGuiTest {
     private static final String ERROR_MESSAGE = "ATTENTION!!!! : On some computers, this test may fail when run on "
